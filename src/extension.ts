04b987a678503195d99e9efd82d929cea9899d35
Ryo ARIMA <ryo.arima@zohomail.jp>
2026-07-05 19:51:44 +0900

    chore: update xaligo renderer to 0.1.11

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import path from "node:path";
 import * as vscode from "vscode";
 
 const xaligoPackageName = "@xaligo/xaligo";
-const xaligoPackageVersionRange = "^0.1.10";
+const xaligoPackageVersionRange = "^0.1.11";
 const xaligoPackageSpec = `${xaligoPackageName}@${xaligoPackageVersionRange}`;
 const installStateKey = "xaligo.packageInstallSpec";
 const previewCommand = "xaligo.openPreview";
